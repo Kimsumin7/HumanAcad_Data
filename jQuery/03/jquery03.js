3f47df34f9eb97e8eed73e255f7b953743606d93
ksmin <ksmin0723@gmail.com>
2023-10-17 14:01:18 +0900

    여러개 파일 삭제

--- jQuery/03/jquery03.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("*").css("border", "2px solid lightblue");
-    $("button").click(function(){
-        $(this).text("클릭됨");
-    });
-    $("p.intro").css("background-color", "yellow");
-    $("p:first").css("font-weight", "bold");
-});
